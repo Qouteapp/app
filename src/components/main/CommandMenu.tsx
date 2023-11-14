1ad2f98dc96efde03e6728a46f65d813f56bc030
serafim.eth <147140426+ulugmer@users.noreply.github.com>
2023-11-14 21:04:06 +0800

    Update CommandMenu.tsx

--- src/components/main/CommandMenu.tsx
+++ src/components/main/CommandMenu.tsx
@@ -8,16 +8,17 @@ import React from 'react';
 // eslint-disable-next-line react/no-deprecated
 import { render } from 'react-dom';
 // eslint-disable-next-line react/no-deprecated
-import { Command, CommandSeparator } from 'cmdk';
+import { Command, CommandSeparator, useCommandState } from 'cmdk';
 import type { FC } from '../../lib/teact/teact';
 import {
   memo, useCallback, useEffect, useState,
 } from '../../lib/teact/teact';
-import { getActions, withGlobal } from '../../global';
+import { getActions, getGlobal, withGlobal } from '../../global';
 
 import type { ApiUser } from '../../api/types';
 
 import { getMainUsername, getUserFirstOrLastName } from '../../global/helpers';
+import { selectTabState } from '../../global/selectors';
 import captureKeyboardListeners from '../../util/captureKeyboardListeners';
 import { throttle } from '../../util/schedulers';
 import renderText from '../common/helpers/renderText';
@@ -31,6 +32,18 @@ import './CommandMenu.scss';
 const cmdkRoot = document.getElementById('cmdk-root');
 const SEARCH_CLOSE_TIMEOUT_MS = 250;
 const NBSP = '\u00A0';
+
+interface SubItemProps {
+  children: React.ReactNode;
+  onSelect: () => void;
+}
+
+const SubItem: React.FC<SubItemProps> = ({ children, onSelect }) => {
+  const search = useCommandState((state) => state.search);
+  // eslint-disable-next-line no-null/no-null
+  if (!search) return null;
+  return <Command.Item onSelect={onSelect}>{children}</Command.Item>;
+};
 
 interface CommandMenuProps {
   topUserIds: string[];
@@ -107,6 +120,32 @@ const CommandMenu: FC<CommandMenuProps> = ({ topUserIds, usersById }) => {
   const { runCommand } = useCommands();
   const [pages, setPages] = useState(['home']);
   const activePage = pages[pages.length - 1];
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchSearchResults = async () => {
+    if (searchQuery.length < 4) {
+      setSearchResults([]);
+      return;
+    }
+
+    setLoading(true);
+    const { setGlobalSearchQuery } = getActions();
+    await setGlobalSearchQuery({ query: searchQuery });
+
+    const globalState = getGlobal();
+    const { globalSearch } = selectTabState(globalState /* tabId */);
+    const localChatIds = globalSearch?.localResults?.chatIds ?? [];
+    const globalChatIds = globalSearch?.globalResults?.chatIds ?? [];
+
+    setSearchResults([...localChatIds, ...globalChatIds]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchSearchResults();
+  }, [searchQuery]);
 
   interface SuggestedContactsProps {
     topUserIds: string[];
@@ -118,44 +157,49 @@ const CommandMenu: FC<CommandMenuProps> = ({ topUserIds, usersById }) => {
     setPages(['home']);
   }, []);
 
-  const SuggestedContacts: FC<SuggestedContactsProps> = ({ topUserIds /* , usersById */ }) => {
-    const { loadTopUsers, openChat, addRecentlyFoundChatId } = getActions();
-    const runThrottled = throttle(() => loadTopUsers(), 60000, true);
+  const { loadTopUsers, openChat, addRecentlyFoundChatId } = getActions();
+  const runThrottled = throttle(() => loadTopUsers(), 60000, true);
 
-    useEffect(() => {
-      runThrottled();
-    }, [loadTopUsers]);
+  useEffect(() => {
+    runThrottled();
+  }, [loadTopUsers]);
 
-    const renderName = (userId: string) => {
-      const name = getUserFirstOrLastName(usersById[userId]) || NBSP;
-      const handle = getMainUsername(usersById[userId]) || NBSP;
-      const renderedText = renderText(name);
-      if (React.isValidElement(renderedText)) {
-        return renderedText;
-      }
+  const handleClick = useCallback((id: string) => {
+    openChat({ id, shouldReplaceHistory: true });
+    setTimeout(() => addRecentlyFoundChatId({ id }), SEARCH_CLOSE_TIMEOUT_MS);
+    close();
+  }, [openChat, addRecentlyFoundChatId]);
+
+  const renderName = (userId: string) => {
+    const name = getUserFirstOrLastName(usersById[userId]) || NBSP;
+    const handle = getMainUsername(usersById[userId]) || NBSP;
+    const renderedText = renderText(name);
+    if (React.isValidElement(renderedText)) {
+      return renderedText;
+    }
+    return (
+      <span>
+        <span className="user-name">{name}</span>
+        <span className="user-handle">{handle}</span>
+      </span>
+    );
+  };
+
+  const SuggestedContacts: FC<SuggestedContactsProps> = ({ topUserIds }) => {
+    const isFirstFive = (userId: string) => topUserIds.indexOf(userId) < 5;
+
+    const renderItem = (userId: string) => {
+      const Component = isFirstFive(userId) ? Command.Item : SubItem;
       return (
-        <span>
-          <span className="user-name">{name}</span>
-          <span className="user-handle">{handle}</span>
-        </span>
+        <Component key={userId} onSelect={() => handleClick(userId)}>
+          <span>{renderName(userId)}</span>
+        </Component>
       );
     };
 
-    const handleClick = useCallback((id: string) => {
-      openChat({ id, shouldReplaceHistory: true });
-      setTimeout(() => addRecentlyFoundChatId({ id }), SEARCH_CLOSE_TIMEOUT_MS);
-      close();
-    }, [openChat, addRecentlyFoundChatId]);
-
     return (
       <Command.Group heading="Suggested contacts">
-        {topUserIds.map((userId) => {
-          return (
-            <Command.Item key={userId} onSelect={() => handleClick(userId)}>
-              <span>{renderName(userId)}</span>
-            </Command.Item>
-          );
-        })}
+        {topUserIds.map(renderItem)}
       </Command.Group>
     );
   };
@@ -224,12 +268,6 @@ const CommandMenu: FC<CommandMenuProps> = ({ topUserIds, usersById }) => {
     usersById: Record<string, ApiUser>;
   }
 
-  interface CreateNewPageProps {
-    handleSelectNewGroup: () => void;
-    handleSelectNewChannel: () => void;
-    handleCreateFolder: () => void;
-  }
-
   const HomePage: React.FC<HomePageProps> = ({
     setPages, commandArchiveAll, topUserIds, usersById,
   }) => {
@@ -247,9 +285,24 @@ const CommandMenu: FC<CommandMenuProps> = ({ topUserIds, usersById }) => {
             <i className="icon icon-archive" /><span>Mark read chats as &quot;Done&quot; (May take ~1-3 min)</span>
           </Command.Item>
         </Command.Group>
+        {loading && <Command.Loading>Fetching chats…</Command.Loading>}
+        {!loading && searchResults.map((chatId) => (
+          <Command.Item key={chatId} onSelect={() => handleClick(chatId)}>
+            {/* Отобразите здесь имя чата, используя chatId */}
+          </Command.Item>
+        ))}
+        {!loading && searchResults.length === 0 && (
+          <Command.Empty>No results found.</Command.Empty>
+        )}
       </>
     );
   };
+
+  interface CreateNewPageProps {
+    handleSelectNewGroup: () => void;
+    handleSelectNewChannel: () => void;
+    handleCreateFolder: () => void;
+  }
 
   const CreateNewPage: React.FC<CreateNewPageProps> = (
     { handleSelectNewGroup, handleSelectNewChannel, handleCreateFolder },
@@ -305,6 +358,7 @@ const CommandMenu: FC<CommandMenuProps> = ({ topUserIds, usersById }) => {
       <Command.Input
         placeholder="Search for command..."
         autoFocus
+        onValueChange={setSearchQuery}
         onKeyDown={(e) => {
           if (e.key === 'Backspace') {
             handleBack();
@@ -313,7 +367,7 @@ const CommandMenu: FC<CommandMenuProps> = ({ topUserIds, usersById }) => {
       />
       <Command.List>
         <Command.Empty>No results found.</Command.Empty>
-        {renderPageContent()}
+        {!loading && renderPageContent()}
       </Command.List>
     </Command.Dialog>
   );
