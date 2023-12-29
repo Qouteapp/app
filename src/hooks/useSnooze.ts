066d15a5ffb3534a496efbee67e2bcb9c971daf9
iower <iower@tutanota.com>
2023-12-29 13:45:39 +0500

    Improve snooze links

--- src/hooks/useSnooze.ts
+++ src/hooks/useSnooze.ts
@@ -2,10 +2,18 @@ import { useMemo } from '../lib/teact/teact';
 import TeactDOM from '../lib/teact/teact-dom';
 import { getActions, getGlobal } from '../global';
 
+import { MAIN_THREAD_ID } from '../api/types/messages';
+
 import {
-  getChatLink, getMainUsername,
+  getChatLink,
 } from '../global/helpers';
-import { selectCurrentChat } from '../global/selectors';
+import {
+  selectChat,
+  selectChatFullInfo,
+  selectCurrentChat,
+  selectCurrentMessageList,
+  selectTopicLink,
+} from '../global/selectors';
 import useLastCallback from './useLastCallback';
 import useSchedule from './useSchedule';
 
@@ -16,34 +24,40 @@ export default function useSnooze() {
   const [requestCalendar, calendar] = useSchedule(true, true);
 
   // Обработчик, который будет вызван после выбора времени в календаре
-  const handleScheduledMessage = useLastCallback((chatId: string, scheduledAt: number, threadId: number = 0) => {
+  const handleScheduledMessage = useLastCallback((chatId: string, scheduledAt: number, threadId?: number) => {
     // eslint-disable-next-line no-console
     console.log('handleScheduledMessage called', { chatId, scheduledAt });
     const global = getGlobal();
-    const currentUserId = global.currentUserId || ''; // Ensure currentUserId is a string
-    const chat = global.chats.byId[chatId];
-    const mainUsername = getMainUsername(chat);
+    const chat = selectChat(global, chatId);
+    if (!chat) {
+      return;
+    }
 
-    let chatLink = chat ? getChatLink(chat) : `https://t.me/${mainUsername}`;
-    let reminderText = `Reminder about the chat: ${chatLink}`;
+    let reminderTitle = '⏰ Reminder about the chat:';
+    let link = getChatLink(chat) || selectChatFullInfo(global, chat.id)?.inviteLink;
 
-    if (threadId !== 0) {
-      chatLink += '/threadId'; // Создаем ссылку на тред
-      reminderText = `Reminder about thread in chat: ${chatLink}`;
+    if (chat?.isForum) {
+      reminderTitle = '⏰ Reminder about thread in chat:';
+      const threadIdSafe = threadId || selectCurrentMessageList(global)?.threadId || MAIN_THREAD_ID;
+      link = selectTopicLink(global, chatId, threadIdSafe);
     }
     sendMessage({
-      text: reminderText,
+      text: [
+        reminderTitle,
+        chat.title,
+        link,
+      ].join('\n'),
       scheduledAt,
       messageList: {
-        chatId: currentUserId,
-        threadId,
+        chatId: global.currentUserId || '',
+        threadId: MAIN_THREAD_ID,
         type: 'thread',
       },
     });
     showNotification({ message: 'You will be reminded!' });
   });
 
-  const snooze = useLastCallback(({ chatId, threadId = 0 }: { chatId?: string; threadId?: number }) => {
+  const snooze = useLastCallback(({ chatId, threadId }: { chatId?: string; threadId?: number }) => {
     const global = getGlobal();
     const currentChatId = selectCurrentChat(global)?.id;
     const chatIdSafe = chatId || currentChatId;
