c2281120fefc195e2b3301b19a72baeb73cc094b
evm97 <j.m.n.90@bk.ru>
2024-01-18 13:17:56 +0300

    feat: [TG-96] add inbox step functionality

--- src/components/ulu/onboarding/steps/Inbox.tsx
+++ src/components/ulu/onboarding/steps/Inbox.tsx
@@ -2,12 +2,17 @@ import type { FC } from '../../../../lib/teact/teact';
 import React, { useCallback, useState } from '../../../../lib/teact/teact';
 import { getActions } from '../../../../global';
 
+import { JUNE_TRACK_EVENTS } from '../../../../config';
 import ContentBlock from '../util/ContentBlock';
 import ContentWrapper from '../util/ContentWrapper';
 import Layout from '../util/Layout';
 import { COLOR_BLUR, COLOR_FOCUS } from '../constants';
 
+import useArchiver from '../../../../hooks/useArchiver';
+import useDone from '../../../../hooks/useDone';
+import { useJune } from '../../../../hooks/useJune';
 import useLang from '../../../../hooks/useLang';
+import { useStorage } from '../../../../hooks/useStorage';
 
 import SvgInboxTelegram from './SvgInboxTelegram';
 import SvgInboxUlu from './SvgInboxUlu';
@@ -16,25 +21,39 @@ import styles from './Inbox.module.scss';
 
 enum InboxOptions {
   Ulu,
-  Telegram,
+  Everywhere,
 }
 
 const Inbox: FC = () => {
   const lang = useLang();
+  const { track } = useJune();
+  const { doneAllReadChats } = useDone();
+  const { setIsArchiveWhenDoneEnabled } = useStorage();
+  const { archiveChats } = useArchiver({ isManual: true });
 
   const [active, setActive] = useState<InboxOptions>(InboxOptions.Ulu);
   const selectInboxUlu = useCallback(() => setActive(InboxOptions.Ulu), []);
-  const selectInboxTelegram = useCallback(() => setActive(InboxOptions.Telegram), []);
+  const selectInboxTelegram = useCallback(() => setActive(InboxOptions.Everywhere), []);
 
   const description = active === InboxOptions.Ulu
     ? lang('OnboardingInboxOptionUluDescription')
-    : lang('OnboardingInboxOptionTelegramDescription');
+    : lang('OnboardingInboxOptionEverywhereDescription');
 
   const { goToOnboardingNextStep } = getActions();
 
+  const markAllReadChatsDone = useCallback(() => {
+    doneAllReadChats();
+    track?.(JUNE_TRACK_EVENTS.USE_MARK_ALL_READ_CHATS_DONE_COMMAND);
+  }, [track, doneAllReadChats]);
+
   const next = useCallback(() => {
+    markAllReadChatsDone();
+    if (active === InboxOptions.Everywhere) {
+      setIsArchiveWhenDoneEnabled(true);
+      archiveChats();
+    }
     goToOnboardingNextStep();
-  }, [goToOnboardingNextStep]);
+  }, [goToOnboardingNextStep, markAllReadChatsDone, active, setIsArchiveWhenDoneEnabled, archiveChats]);
 
   return (
     <Layout
@@ -56,12 +75,12 @@ const Inbox: FC = () => {
           </div>
         </ContentBlock>
         <ContentBlock
-          active={active === InboxOptions.Telegram}
-          title={lang('OnboardingInboxOptionTelegramTitle')}
+          active={active === InboxOptions.Everywhere}
+          title={lang('OnboardingInboxOptionEverywhereTitle')}
           onClick={selectInboxTelegram}
         >
           <div className={styles.iconWrapper}>
-            <SvgInboxTelegram color={active === InboxOptions.Telegram ? COLOR_FOCUS : COLOR_BLUR} />
+            <SvgInboxTelegram color={active === InboxOptions.Everywhere ? COLOR_FOCUS : COLOR_BLUR} />
           </div>
         </ContentBlock>
       </ContentWrapper>
