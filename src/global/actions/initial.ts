5c2acfad2bf3ba50d176259601464fc37d7fa073
evm97 <j.m.n.90@bk.ru>
2024-01-24 19:38:25 +0300

    fix: [TG-96] fix missing action set

--- src/global/actions/initial.ts
+++ src/global/actions/initial.ts
@@ -2,6 +2,7 @@ import './ui/initial';
 import './ui/settings';
 import './api/initial';
 import './api/settings';
+import './api/users';
 import './apiUpdaters/initial';
 import './ui/ulu/onboarding';
 import './ui/ulu/workspaces';
